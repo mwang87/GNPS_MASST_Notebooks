bd0f92daac9ba986d7b7137b60fb9b510793b7fb
Mingxun Wang <mwang87@gmail.com>
2021-01-11 14:56:27 -0800

    creating a network

--- masst-network/masst_network.py
+++ masst-network/masst_network.py
@@ -19,7 +19,12 @@ all_datasets = requests.get("https://massive.ucsd.edu/ProteoSAFe/datasets_json.j
 all_node_usi_list = []
 
 # Source MASST USI
-all_node_usi_list.append("mzspec:GNPS:TASK-c6b2797224f34d819d20dd7af622bc6b-spectra/:scan:1")
+output_dict = {}
+output_dict["usi"] = "mzspec:GNPS:TASK-c6b2797224f34d819d20dd7af622bc6b-spectra/:scan:1"
+output_dict["dataset"] = "QUERY"
+output_dict["scan"] = 1
+
+all_node_usi_list.append(output_dict)
 
 # Getting all the MASST data
 for dataset in dataset_matches:
@@ -43,7 +48,11 @@ for dataset in dataset_matches:
     for edge in filtered_edges.to_dict(orient="records"):
         cluster = edge["Node2"]
         usi = "mzspec:GNPS:TASK-{}-speccontinuous/speccontinuous-00000.mgf:scan:{}".format(continuous_id["jobs"][0]["task"], cluster)
-        all_node_usi_list.append(usi)
+        output_dict = {}
+        output_dict["usi"] = usi
+        output_dict["dataset"] = filtered_dataset[0]["dataset"]
+        output_dict["scan"] = cluster
+        all_node_usi_list.append(output_dict)
 
 print(len(all_node_usi_list), "Total Spectra")
 
@@ -51,15 +60,22 @@ print(len(all_node_usi_list), "Total Spectra")
 from ming_spectrum_library import Spectrum
 import spectrum_alignment
 all_spectra_list = []
-for usi in all_node_usi_list:
+for usi_dict in all_node_usi_list:
+    print(usi_dict)
+
+    usi = usi_dict["usi"]
+    display_information = "{}:{}".format(usi_dict["dataset"], usi_dict["scan"])
+
     url = "https://metabolomics-usi.ucsd.edu/json/?usi={}".format(usi)
     spectrum_json = requests.get(url).json()
 
-    spectrum = Spectrum("", usi, usi, spectrum_json["peaks"], spectrum_json["precursor_mz"], 1, 2)
+    spectrum = Spectrum("", display_information, display_information, spectrum_json["peaks"], spectrum_json["precursor_mz"], 1, 2)
+    spectrum.dataset = usi_dict["dataset"]
+    spectrum.usi = usi_dict["usi"]
     all_spectra_list.append(spectrum)
 
 min_score = 0.7
-min_matched_peaks = 4
+min_matched_peaks = 5
 
 # Let's create a network now
 G = nx.Graph()
@@ -67,6 +83,9 @@ from tqdm import tqdm
 for i, spectrum1 in tqdm(enumerate(all_spectra_list)):
     for j, spectrum2 in enumerate(all_spectra_list):
         if i <= j:
+            continue
+
+        if spectrum1.usi == spectrum2.usi:
             continue
         
         # Doing a network here
@@ -77,8 +96,19 @@ for i, spectrum1 in tqdm(enumerate(all_spectra_list)):
         if len(reported_alignments) < min_matched_peaks:
             continue
 
-        G.add_edge(spectrum1.scan, spectrum2.scan)
+        G.add_edge(spectrum1.scan, spectrum2.scan, cosine_score=total_score, matched_peaks=len(reported_alignments))
+
+        # Adding Node Attributes
+        G.nodes[spectrum1.scan]["mz"] = spectrum1.mz
+        G.nodes[spectrum2.scan]["mz"] = spectrum2.mz
+        G.nodes[spectrum1.scan]["dataset"] = spectrum1.dataset
+        G.nodes[spectrum2.scan]["dataset"] = spectrum2.dataset
 
 import matplotlib.pyplot as plt
+import molecular_network_filtering_library
+
+molecular_network_filtering_library.filter_top_k(G, 10)
+
 nx.draw(G, with_labels=True, font_weight='bold')        
 plt.savefig("Graph.png", format="PNG")
+nx.write_graphml(G, "test.graphml")
